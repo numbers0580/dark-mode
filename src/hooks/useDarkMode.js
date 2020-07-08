4840e57f0eb0c38d71ab2eed95cd2c25f9348af7
numbers0580 <koreankid18@yahoo.com>
2020-07-08 13:49:58 -0600

    Completed through Step 3 so far. Nothing seemed to have changed in React App, but also nothing seemed to break, so I'll take that as good news.

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = () => {
+    const [toggleDark, switchDarkMode] = useLocalStorage(key, initialValue);
+
+    useEffect(() => {
+        if(toggleDark) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }, []);
+
+    return [toggleDark, switchDarkMode];
+};
+
+export default useDarkMode;
